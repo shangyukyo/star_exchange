bdd973254e2a713a803a11228ca7d02b9d4b8b0f
hs-info <sdzy890819@126.com>
2017-06-20 13:41:23 +0800

    fixbug

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, "star_exchange"
 set :repo_url, "git@github.com:shangyukyo/star_exchange.git"
 
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
