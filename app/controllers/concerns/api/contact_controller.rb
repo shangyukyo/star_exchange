8cdc4e97dbd249b4a881f10d02ed5c3e96961c8c
hs-info <sdzy890819@126.com>
2017-07-03 10:29:32 +0800

    fixbug

--- app/controllers/concerns/api/contact_controller.rb
+++ app/controllers/concerns/api/contact_controller.rb
@@ -2,10 +2,10 @@ class Api::ContactController< Api::ApplicationController
   
   def create
   	c = Contact.new
-  	c.name = params[:name]
+  	c.name = params[:name] || params["First_Name"]
   	c.email = params[:email]
-    c.interested = params[:What_program_are_you_interested_in]
-    c.message = params[:nachricht]
+    c.interested = params[:What_program_are_you_interested_in] || params["Please_select_the_program_you're_interested_in"]
+    c.message = params[:nachricht] || params["Message"]
   	c.save
   	redirect_to "/client/thanks_for_application.html"
 
