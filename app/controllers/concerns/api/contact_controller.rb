545dfefb019db1d895690331c4b93c9450d51b60
hs-info <sdzy890819@126.com>
2017-08-05 09:00:19 +0800

    修改

--- app/controllers/concerns/api/contact_controller.rb
+++ app/controllers/concerns/api/contact_controller.rb
@@ -5,7 +5,7 @@ class Api::ContactController< Api::ApplicationController
   	c.name = params[:name] || params["First_Name"]
   	c.email = params[:email]
     c.interested = params[:What_program_are_you_interested_in] || params["Please_select_the_program_you're_interested_in"]
-    c.message = params[:nachricht] || params["Message"]
+    c.message = params[:nachricht] || params["Message"] || params["Deine_Nachricht"]
   	c.save
   	redirect_to "/client/thanks_for_application.html"
 
